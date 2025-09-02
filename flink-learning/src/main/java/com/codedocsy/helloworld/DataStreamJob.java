06f28ce492aded596ae489ee2a028647755a7b9c
Jianxingxu63 <Jianxingxu63@gmail.com>
2025-09-02 18:27:06 +0800

    add fink learning code

---
+++ flink-learning/src/main/java/com/codedocsy/helloworld/DataStreamJob.java
@@ -0,0 +1,32 @@
+
+package com.codedocsy.helloworld;
+
+import org.apache.flink.api.common.functions.FilterFunction;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+
+public class DataStreamJob {
+    public static void main(String[] args) throws Exception {
+        AtomicInteger counter = new AtomicInteger(0);
+        final StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+        System.out.println("默认执行的并发度" + env.getParallelism());
+        env.fromSequence(1, 10)
+                .filter(new FilterFunction<Long>() {
+                    @Override
+                    public boolean filter(Long input) throws Exception {
+                        int count = counter.incrementAndGet();
+                        System.out.println("输入的数据: " + input +
+                                ", 当前执行计数: " + count +
+                                ", 执行线程ID: " + Thread.currentThread().getId());
+                        // 模拟耗时处理
+                        Thread.sleep(500);
+                        return true;
+                    }
+                });
+        env.execute("Flink Java API Skeleton");
+        Thread.sleep(10000);
+        System.out.println("执行的总次数:" + counter.get());
+    }
+}
